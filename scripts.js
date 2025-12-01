835d6a7e9bce8a3f32f61e3690e1d5be35ae8aca
qanszer <maluendajohnsteven@gmail.com>
2025-12-01 12:18:59 +0800

    feat: add popup form for report button
    the form is there, but it is yet to be stylized and configured according to its purpose.

--- scripts.js
+++ scripts.js
@@ -7,3 +7,13 @@ function openNav() {
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
 }
+
+
+
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
